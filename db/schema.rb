147f8bcb8637e6f7812b3fa4fa4ddbd48d741033
Tish Chungoora <tishal82@gmail.com>
2019-12-30 11:51:40 +0000

    Migration for users now includes email column instead of business_email

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2019_12_27_232614) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "business_email"
+    t.string "email"
     t.string "password_digest"
     t.string "first_name"
     t.datetime "created_at", precision: 6, null: false
