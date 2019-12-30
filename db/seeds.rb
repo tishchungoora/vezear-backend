4e9177828dd1e322157739fb964868e9838f571b
Tish Chungoora <tishal82@gmail.com>
2019-12-30 14:00:30 +0000

    Added top level business categories in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@ require 'csv'
 
 Product.destroy_all
 ProductCategory.destroy_all
+BusinessCategory.destroy_all
+User.destroy_all
 
 puts "Pre-existing data cleared"
 
@@ -25,11 +27,16 @@ puts "Products and product categories created"
 
 # Load business categories
 
+categories = ["Education", "Food & drink", "Travel and transport", "Science, technology and engineering", "Finance", "Media/news company", "Legal", "Non-governmental organisation (NGO)", "Sport & recreation", "Agriculture", "Medical & health", "Shopping & retail", "Automotive, aircraft and boat", "Hotel and B&B", "Non-profit organisation", "Advertising/marketing", "Arts & entertainment", "Property", "Public & government service", "Local service", "Beauty, cosmetic & personal care"]
+
 subcategories = ["Bakery", "Bagel shop", "Bar", "Bottled water company", "Brewery", "Bubble tea shop", "Butcher's shop", "Cafeteria", "Café", "Caterer", "Cheese shop", "Convenience store", "Deli", "Dessert shop", "Distillery", "Doughnut shop", "Farmers' market", "Food consultant", "Food delivery service", "Food service distributor", "Food stall", "Food wholesaler", "Personal chef", "Restaurant", "Sandwich shop", "Smoothie and juice bar", "Snack van", "Supermarket/Convenience store", "Wine, beer and spirits shop", "Winery/Vineyard"]
 
+categories.each do |cat|
+    BusinessCategory.create(name: cat)
+end
+
 subcategories.each do |subcat|
-    BusinessCategory.create(name: subcat, parent: BusinessCategory.find_or_create_by(name: 'Food and Drink'))
+    BusinessCategory.create(name: subcat, parent: BusinessCategory.find_or_create_by(name: "Food & drink"))
 end
 
 puts "Business categories created"
-
